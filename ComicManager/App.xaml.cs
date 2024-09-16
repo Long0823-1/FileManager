b76f54e20e7c41b5b8e831d530e8ed7e519016c7
とあがく <users.noreply.github.com>
2024-09-16 22:09:56 +0900

    プロジェクト ファイルを追加します。

---
+++ ComicManager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ComicManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public MainViewModel MainViewModel = new MainViewModel();
+    }
+}
---
+++ ComicManager/GetFilesList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicManager
+{
+    public class GetFilesList
+    {
+        public GetFilesList() 
+        {
+            vm = (App.Current as App).MainViewModel;
+        }
+        MainViewModel vm;
+        public Task<ObservableCollection<string>> FilesList(string Path = @$"Z:\ダウン\本\mu\ふ\ふ\の\ふ\エロ漫画")
+        {
+            vm.Path = Path;
+            vm.FilesList = new ObservableCollection<string>();
+            var files = Directory.GetFileSystemEntries(Path);
+            foreach (var file in files)
+            {
+                vm.FilesList.Add(file);
+            }
+            return null;
+        }
+    }
+}
